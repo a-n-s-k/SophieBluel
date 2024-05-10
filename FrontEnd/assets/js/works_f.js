80e66af65c8e787b602cbb49ec5c7de1a1ecc719
Sewa <adjevinsk@gmail.com>
2024-05-10 23:50:17 +0200

    Quelques modifications JS

--- FrontEnd/assets/js/works_f.js
+++ FrontEnd/assets/js/works_f.js
@@ -1,31 +1,66 @@
-/* async function getWorks(file) {
-    let myObject = await fetch(file);
-    let myText = await myObject.text();
-    myDisplay(myText);
-} */
-//liste de tous les projets
-async function getWorks(){
-    let projets = await fetch('http://localhost:5678/api/works', { 
-        method: 'GET' 
-    });
-    let reponseprojets = await projets.json();
-    return reponseprojets;
+function statutPromesse (response) {
+  if (response.status >= 200 && response.status < 300) {
+  return Promise.resolve(response)
+  } else {
+  return Promise.reject(new Error(response.statusText))
+  }
 }
 
-const lesProjets = getWorks();
+function jsonPromesse (response) {
+  return response.json()
+}
+
+/* function creationElements () {
+      // Création de l'élément HTML figure
+      const figureProjet = document.createElement("figure");
+      // Création de l'élément HTML img
+     const imageProjet = document.createElement("img");
+     // Création de l'élément HTML figcaption
+     const figcaptionProjet = document.createElement("figcaption");
+      return;
+} */
+/* function creationAtributsElements () {
+  // Création de l'attibut src de l'élément HTML img et ajout de son contenu
+ imageProjet.src = projet.imageUrl;
+  // Création de l'attibut alt de l'élément HTML img et ajout de son contenu
+ imageProjet.alt = projet.title;
+ // Ajout du contenu entre les balises ouvrante et fermante figcaption
+ figcaptionProjet.textContent = projet.title;
+ figureProjet.append(
+     imageProjet,
+     figcaptionProjet
+ );
+ laGalerie.appendChild(figureProjet);
+  return;
+} */
+/* function creationElements () {
+  // Création de l'élément HTML figure
+  const figureProjet = document.createElement("figure");
+  // Création de l'élément HTML img
+ const imageProjet = document.createElement("img");
+  // Création de l'attibut src de l'élément HTML img et ajout de son contenu
+ imageProjet.src = projet.imageUrl;
+  // Création de l'attibut alt de l'élément HTML img et ajout de son contenu
+ imageProjet.alt = projet.title;
+ // Création de l'élément HTML figcaption
+ const figcaptionProjet = document.createElement("figcaption");
+ // Ajout du contenu entre les balises ouvrante et fermante figcaption
+ figcaptionProjet.textContent = projet.title;
+ figureProjet.append(
+     imageProjet,
+     figcaptionProjet
+ );
+ laGalerie.appendChild(figureProjet);
+  return;
+} */
+
+function affichageProjets (data) {
+  const laGalerie = document.querySelector("div.gallery");
+  for (const projet of data) {
 
 
-/* let copieprojets = [];
-lesProjets.forEach(function (item) {
-    copieprojets.push(item);
-});
-console.log(copieprojets); */
 
-
-/* lesProjets.forEach((element) => console.log(element)); */
-
-/* const laGalerie = document.querySelector("div.gallery");
-for (const projet in lesProjets) {
+    
     // Création de l'élément HTML figure
     const figureProjet = document.createElement("figure");
      // Création de l'élément HTML img
@@ -38,25 +73,72 @@ for (const projet in lesProjets) {
     const figcaptionProjet = document.createElement("figcaption");
     // Ajout du contenu entre les balises ouvrante et fermante figcaption
     figcaptionProjet.textContent = projet.title;
-
     figureProjet.append(
         imageProjet,
         figcaptionProjet
     );
     laGalerie.appendChild(figureProjet);
-  } */
+  }
+  return;
+}
+
+function affichageFiltreCategorie (data) {
+  const laGalerie = document.querySelector("div.gallery");
+  for (const projet of data) {
+    // Création de l'élément HTML figure
+    if (projet.categoryId === 3){
+      const figureProjet = document.createElement("figure");
+      // Création de l'élément HTML img
+     const imageProjet = document.createElement("img");
+      // Création de l'attibut src de l'élément HTML img et ajout de son contenu
+     imageProjet.src = projet.imageUrl;
+      // Création de l'attibut alt de l'élément HTML img et ajout de son contenu
+     imageProjet.alt = projet.title;
+     // Création de l'élément HTML figcaption
+     const figcaptionProjet = document.createElement("figcaption");
+     // Ajout du contenu entre les balises ouvrante et fermante figcaption
+     figcaptionProjet.textContent = projet.title;
+     figureProjet.append(
+         imageProjet,
+         figcaptionProjet
+     );
+     laGalerie.appendChild(figureProjet);
+    }  
+  }
+  return;
+  
+}
+
+
+/* const fruits = [
+  {name: 'strawberry', color: 'red'},
+  {name: 'chery', color: 'red'},
+  {name: 'apple', color: 'green'},
+  {name: 'grape', color: 'yellow'},
+]
+let redFruits = [];
+
+fruits.forEach (function (fruit) {
+  if (fruit.color === 'red') redFruits.push(fruit.name);
+})
+
+console.log(redFruits);
+
+// output: strawberry,chery */
 
 
 
-/* const laGalerie = document.querySelector("div.gallery");
-fetch('http://localhost:5678/api/works')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error, status = ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
+
+
+
+
+function lesProjets () {
+  fetch('http://localhost:5678/api/works')
+  .then(statutPromesse)
+  .then(jsonPromesse)
+  .then(affichageProjets)
+  /* .then(function(data) {
+    const laGalerie = document.querySelector("div.gallery");
     for (const projet of data) {
       // Création de l'élément HTML figure
       const figureProjet = document.createElement("figure");
@@ -77,18 +159,12 @@ fetch('http://localhost:5678/api/works')
       );
       laGalerie.appendChild(figureProjet);
     }
-  })
-  .catch((error) => {
-    const p = document.createElement("p");
-    p.appendChild(document.createTextNode(`Error: ${error.message}`));
-    // document.body.insertBefore(p, laGalerie);
+  }) */
+  .catch(function(error) {
+  console.log("Il y a un problème. Status Code:", error);
   });
- */
-
-  
-
-  
-
-
+  return;
+}
+lesProjets ();
 
 
