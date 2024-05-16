cdd0c52e089f2d8bea6d52a58d85137c02e68b8a
Sewa <adjevinsk@gmail.com>
2024-05-16 21:16:04 +0200

    Des modifications sur tous les fichiers du site

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,31 +1,51 @@
-import {statutPromesse, jsonPromesse} from "./mon-module.js";
+import {setCookie} from "./set-get-check-cookies.js";
+
+const array = new Uint32Array(1);
+self.crypto.getRandomValues(array);
+for (const num of array) {
+  value = num;
+}
+const cookievalue = value;
+// alert(cookievalue);
+// console.log(cookievalue);
+
+/* let getActive = browser.tabs.query({ active: true, currentWindow: true });
+getActive.then(setCookie);
+
+function setCookie(tabs) {
+  browser.cookies.set({
+    url: tabs[0].url,
+    name: loginEmail,
+    value: cookievalue,
+  });
+}
+console.log(setCookie(tabs)); */
 
 const loginEmail = document.getElementById("email");
 const loginPassword = document.getElementById("password");
+let auth = btoa(`${loginEmail}:${loginPassword}`);
+fetch('http://localhost:5678/api/users/login', {
+  method: "POST",
+	headers: {
+		'Authorization': `Basic ${auth}`
+	}
+})
+.then(function (response) {
+	if (response.ok) {
+		return response.json();
+	}
+	throw response;
+})
+.then( 
+  `setCookie(loginEmail, cookievalue),
+  document.location.replace("./index.html")`
+)
+.catch(function (error) {
+	console.warn(error);
+});
 
-function laConnexion () {
-  fetch('http://localhost:5678/api/users/login', {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          email: loginEmail, 
-          password: loginPassword, 
-        }) 
-      })
-  .then(statutPromesse)
-  .then(jsonPromesse)
 
-  .then(console.log(data))
-  .catch(function(error) {
-  console.log("Il y a un problème. Status Code:", error);
-  });
-  return;
-}
-laConnexion ();
+
                     
 
 
--- FrontEnd/assets/js/recuperer-cookie.js
+++ FrontEnd/assets/js/recuperer-cookie.js
@@ -1,29 +1,75 @@
-/**
-* Pour recupérer les données associées à un coookie
-* @param {string} name est la variable du nom du cookie à recupérer
-* @return {string|null} le type de la valeur qui sera retournée
-*/
-function getCookie(name) {
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
+
+
+
+
+
+
+
+
+
+// Debut - Déclaration et affectatiopn de la variable cookiename
+const cookiename = "cookieSophieBluel";
+// Fin - Déclaration et affectatiopn de la variable cookiename
+
+// Debut - Fonction de recupération du cookie
+export function getCookie(cookiename) {
     const cookies = document.cookie.split(";");
-    const value = cookies
-    .find(c => c.startsWith(name + "="))
+    cookievalue = cookies
+    .find(c => c.startsWith(cookiename + "="))
     ?.split("=")[1];
-    if (value === undefined) {
+    if (cookievalue === undefined) {
         return null;
     }
-    return decodeURIComponent(value);
-
+    return decodeURIComponent(cookievalue);
 }
+// Fin - Fonction de recupération du cookie
 
-
-/**
-* Pour créer ou modifier la valeur d'un cookie avec une durée spécifique
-* @param {string} name est la variable du nom du cookie
-* @param {string} value est la variable de la valeur du cookie
-* @param {number} days est la durée de vie du cookie
-*/
-function setCookie(name, valie, days) {
-    const days = new Date();
-    date.setDate(date.getDate() + days);
-    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${date.toUTCString()};`
+// Debut - Fonction de création du cookie
+export function setCookie() {
+    // Debut - Construction de la valeur à affecter au cookie
+    const array = new Uint32Array(1);
+    self.crypto.getRandomValues(array);
+    for (const num of array) {
+        const cookievalue = num;
+        document.cookie = `${cookiename}=${cookievalue};SameSite=None; Secure; path=/`;
+    }
+    // Fin - Construction de la valeur à affecter au cookie
+    // document.cookie = `${cookiename}=${cookievalue};SameSite=None; Secure; path=/`
 }
+// Fin - Fonction de création du cookie
+
+
